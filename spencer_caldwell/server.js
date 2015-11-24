c12ed4ad5189f731abfa60d21a838e37f1153d22
Spencer Caldwell <spencer.caldwell@gmail.com>
2015-11-24 11:53:50 -0800

    started to add the FS commands for Angular

--- spencer_caldwell/server.js
+++ spencer_caldwell/server.js
@@ -1,11 +1,26 @@
 var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
+var fs = require('fs');
 var unicornsRouter = require(__dirname + '/routes/unicorns_routes')
 
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/unicorn_stream_dev');
+// mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/unicorn_stream_dev');
 
-app.use('/api', unicornsRouter);
+// app.use('/api', unicornsRouter);
+
+app.get('/:filename', function(req, res) {
+  fs.start(__dirname + '/build/' + req.params.filename, function(err, stats) {
+    if(err) {
+      console.log(err);
+      return next();
+    }
+
+    if (!stats.isFile()) return next();
+
+    var file = fs.createReadStream(__dirname + '/build/' + req.params.filename);
+    file.pipe(res);
+  });
+});
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('server up');
